284f6b1efe024b9406eb9e817275f664d3e288f8
田中龍之介 <ry-tanaka@sios.com>
2023-11-11 21:58:23 +0900

    fix:タイトル入れ替え

--- src/components/pages/ChartPage.tsx
+++ src/components/pages/ChartPage.tsx
@@ -14,13 +14,13 @@ export const ChartPage = () => {
     <>
       <main className="my-10 flex h-full w-full max-w-5xl grow flex-col items-center justify-start gap-10 font-dela">
         <ul className="flex w-full flex-row justify-around text-xl md:text-3xl">
-          <li className="flex flex-row items-center gap-2  text-[#22C55E]">
-            <GiBamboo className="h-12 w-12" />
-            タ〇ノコの里
-          </li>
           <li className="flex flex-row items-center gap-2  text-[#F59E0B]">
             <GiGrassMushroom className="h-12 w-12" />
             き〇この山
+          </li>
+          <li className="flex flex-row items-center gap-2  text-[#22C55E]">
+            <GiBamboo className="h-12 w-12" />
+            タ〇ノコの里
           </li>
         </ul>
         <div className="flex w-full flex-col-reverse items-center gap-1 md:flex-row md:gap-3">
