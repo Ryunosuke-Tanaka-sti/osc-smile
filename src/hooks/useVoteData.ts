d7a10d8b31049e78f8b37f90eb828f87a8c89427
田中龍之介 <ry-tanaka@sios.com>
2023-11-11 21:53:54 +0900

    fix:当日用の修正

--- src/hooks/useVoteData.ts
+++ src/hooks/useVoteData.ts
@@ -3,6 +3,8 @@ import useSWR from 'swr';
 import { GetTotalVoteData } from '@/api/votingAPI';
 
 export const useVoteData = () => {
-  const { data: totalVote, isLoading: isLoadingTotalVote } = useSWR('totalVote', GetTotalVoteData);
+  const { data: totalVote, isLoading: isLoadingTotalVote } = useSWR('totalVote', GetTotalVoteData, {
+    refreshInterval: 10000,
+  });
   return { totalVote, isLoadingTotalVote };
 };
